3cb160119ac9b5f777f811cbd7300655b3d2d495
Felix Kaasa <68005825+Epirius@users.noreply.github.com>
2023-10-22 19:06:59 +0200

    fixing bug where you could scroll below player (#17)

--- src/pages/podcast/[slug].tsx
+++ src/pages/podcast/[slug].tsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import { dbUrl } from "~/utils/backendInfo";
-import {zMetaData, zEpisodeData} from "~/types/podcastTypes";
-import type {EpisodeData, MetaData} from "~/types/podcastTypes";
+import { zMetaData, zEpisodeData } from "~/types/podcastTypes";
+import type { EpisodeData, MetaData } from "~/types/podcastTypes";
 import * as AspectRatio from "@radix-ui/react-aspect-ratio";
-import {EpisodeList} from "~/components/EpisodeList";
+import { EpisodeList } from "~/components/EpisodeList";
 
 const PodcastPage = () => {
   const slug = useRouter().query.slug;
@@ -25,7 +25,7 @@ const PodcastPage = () => {
       const data = zEpisodeData.parse(await res.json());
       if (!data) return;
       setEpisodeData(data);
-    }
+    };
     void Promise.allSettled([getEpisodedata(), getMetadata()]);
   }, [slug]);
 
@@ -35,32 +35,42 @@ const PodcastPage = () => {
 
   // if (!podcast) return <p>could not load page</p>;
   return (
-    <div className="relative h-full px-12 pb-16 flex flex-col overflow-y-auto">
+    <div className="relative px-12 flex flex-col">
       {/* Podcast metadata display */}
-        <div className=" md:w-full  self-center md:self-start py-8 md:py-12 flex flex-col md:flex-row gap-4 md:gap-12">
-          <div className="relative w-[200px] h-[200px] md:w-[300px] md:h-[300px] flex-none">
+      <div className=" md:w-full  self-center md:self-start py-8 md:py-12 flex flex-col md:flex-row gap-4 md:gap-12">
+        <div className="relative w-[200px] h-[200px] md:w-[300px] md:h-[300px] flex-none">
           <AspectRatio.Root ratio={1}>
-            {metadata?.image_url && <Image
-              className="rounded-2xl"
-              src={metadata.image_url}
-              alt={"cover image for the podcast"}
-              width={300}
-              height={300}
-              priority={true}
-            />}
-            {metadata?.image_url && <div className="bg-gray-200 rounded-2xl"/>}
+            {metadata?.image_url && (
+              <Image
+                className="rounded-2xl"
+                src={metadata.image_url}
+                alt={"cover image for the podcast"}
+                width={300}
+                height={300}
+                priority={true}
+              />
+            )}
+            {metadata?.image_url && <div className="bg-gray-200 rounded-2xl" />}
           </AspectRatio.Root>
-          </div>
-          <div className="flex flex-col flex-shrink max-w-xl xl:max-w-4xl  md:h-[300px]">
-            <h1 className="text-3xl font-bold">{metadata ? metadata.title : "Podcast"}</h1>
-            {metadata?.description && <p className="hidden md:flex overflow-y-hidden" style={{overflowWrap: "anywhere", wordBreak: "break-word"}}>{metadata.description}</p>}
-          </div>
         </div>
+        <div className="flex flex-col flex-shrink max-w-xl xl:max-w-4xl  md:h-[300px]">
+          <h1 className="text-3xl font-bold">
+            {metadata ? metadata.title : "Podcast"}
+          </h1>
+          {metadata?.description && (
+            <p
+              className="hidden md:flex overflow-y-hidden"
+              style={{ overflowWrap: "anywhere", wordBreak: "break-word" }}
+            >
+              {metadata.description}
+            </p>
+          )}
+        </div>
+      </div>
       {/* Episode list */}
       {/*<div className="relative flex-grow">*/}
 
-
-      {episodeData && <EpisodeList data={episodeData}/>}
+      {episodeData && <EpisodeList data={episodeData} />}
 
       {/*<EpisodeList podcast={podcast} metaData={podcastData} />*/}
       {/*</div>*/}
